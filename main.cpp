f57674d259057210258d935785bbd82824f6673a
Linyuan Zhong <zhonglinyuan@outlook.com>
2024-08-19 23:53:34 +0800

    tga

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "tgaimage.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const int screen_with = 1024;
+const int screen_height = 768;
+TGAImage image(screen_with, screen_height, TGAImage::RGB);
+
+
+int CanvasToViewport(int x, int y, int* vx, int* vy)
+{
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	for (int x = 0; x < screen_with; x++)
+	{
+		for (int y = 0; y < screen_height; y++)
+		{
+			image.set(x, y, red);
+		}
+	}
+	image.write_tga_file("output.tga");
+	return 0;
+}
